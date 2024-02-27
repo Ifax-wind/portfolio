03fe65993162571d6f2316b5c970a1aebdd7e8e0
Ifax-wind <aravindr.offi@gmail.com>
2024-02-27 09:49:37 +0530

    -

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,24 +1,10 @@
 import TodoList from '../assets/to-do-list.png'
-import Pro2 from '../assets/portfolio.png'
-import Calculator from '../assets/Calculator.png'
 
 export const ProjectData = [
     {
         PName : 'Todo List',
-        Demo : 'https://kaviyarasan600.github.io/Todo-List/',
-        Score: 'https://github.com/Kaviyarasan600/Todo-List',
-        img : `${TodoList}`,
-    },
-    {
-        PName : 'Portfolio',
-        Demo : 'https://kaviyarasan600.github.io/Portfolio/',
-        Score: 'https://github.com/Kaviyarasan600/Todo-List',
-        img : `${Pro2}`,
-    },
-    {
-        PName : 'Calculator',
-        Demo : 'https://kaviyarasan600.github.io/Calculator/',
-        Score: 'https://github.com/Kaviyarasan600/Calculator.git',
-        img : `${Calculator}`,
-    },
+        Demo : 'https://ifax-wind.github.io/todolist/',
+        Score: 'https://github.com/Ifax-wind/todolist.git',
+        img : `${TodoList}`
+    }
 ]
